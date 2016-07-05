835ff99d3152c0abe1e80cbc14e9bf4eeb918367
webgrig <grigoriev.ua@gmail.com>
2016-07-05 14:38:10 +0300

    Отправка письма добавил дату

--- scripts/send.php
+++ scripts/send.php
@@ -40,7 +40,9 @@ $to = [
 	'grigoriev.ua@gmail.com',
 	'webgrig@mail.ru'
 ];
-$message = "Заявка \r\n";
+///////////////
+$date = date("d-m-y H:i:s");
+$message = "Заявка ($date)\r\n";
 foreach ($resArr as $key => $value) {
 	if ($key == "name") {
 		$message .= "Имя: ".$resArr['name']."\r\n";
@@ -74,7 +76,7 @@ $Cc = array_walk($to, function($item, $key) use (&$CcArr){
 	}
 });
 $CcStr = implode(";", $CcArr);
-$subject = "Заявка";
+$subject = "Заявка ($date)";
 $headers = 'Content-type: text/plain; charset=utf-8' . "\r\n" . "From: <{$_SERVER['HTTP_HOST']}>\r\n" . 'Cc: ' .$CcStr.  "\r\n" . 'X-Mailer: PHP/' . phpversion();
 //echo $headers;exit;
 if(mail($to[0], $subject, $message, $headers)){
